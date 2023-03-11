7ce179517f1e5e4085e723dc9a9de708f90ee86a
jang-han <56466024+jang-han@users.noreply.github.com>
2023-03-12 01:48:15 +0900

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Test;
+
+class TestController extends Controller
+{
+    //
+    public function index()
+    {
+
+        $values = Test::all();
+
+        // die + var_dump
+        // dd($values);
+
+        return view('tests.test', compact('values'));
+    }
+}
